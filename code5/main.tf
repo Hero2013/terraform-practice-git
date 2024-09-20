6283655ef51092a3370d36737028875f9a151b1b
MmaChiugo <heromatica@gmail.com>
2024-09-19 22:51:45 -0500

    terraform code

---
+++ code5/main.tf
@@ -0,0 +1,14 @@
+resource "aws_lightsail_instance" "custom" {
+  name              = "Dev_server"
+  availability_zone = "us-east-1b"
+  blueprint_id      = "amazon_linux_2"
+  bundle_id         = "nano_3_0"
+  key_pair_name     = "some_key_name"
+  user_data         = "sudo yum install -y httpd && sudo systemctl start httpd && sudo systemctl enable httpd && echo '<h1>Deployed via Terraform</h1>' | sudo tee /var/www/html/index.html"
+  tags ={
+    Team = "DevOps"
+    env  = "dev"
+    create_by = "terraform"
+  }
+}
+
